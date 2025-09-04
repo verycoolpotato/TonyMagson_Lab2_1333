4fe9ce0d116b231f37c2b195d788f71a24bf52b0
verycoolpotato <100438125+verycoolpotato@users.noreply.github.com>
2025-09-03 21:38:14 -0700

    Created the project

---
+++ GD13_TonyMagson_Lab2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GD13_TonyMagson_Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
